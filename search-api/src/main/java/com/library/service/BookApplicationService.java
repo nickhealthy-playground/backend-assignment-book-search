eb974a01f35a2d0ea4d8ab2262c0342c859e634f
nick <alskadmlcraz1@gmail.com>
2026-09-14 21:49:03 +0900

    [LS-9] 고가용성설계 - event publisher 틀적용

--- search-api/src/main/java/com/library/service/BookApplicationService.java
+++ search-api/src/main/java/com/library/service/BookApplicationService.java
@@ -4,25 +4,32 @@ import com.library.controller.response.PageResult;
 import com.library.controller.response.SearchResponse;
 import com.library.controller.response.StatResponse;
 import com.library.entity.DailyStat;
+import com.library.service.event.SearchEvent;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class BookApplicationService {
     private final BookQueryService bookQueryService;
-    private final DailyStatCommandService dailyStatCommandService;
     private final DailyStatQueryService dailyStatQueryService;
+    private final ApplicationEventPublisher eventPublisher;
 
     public PageResult<SearchResponse> search(String query, int page, int size) {
         // 외부 api 호출 -> 통계데이터 저장 -> api 호출값 응답
         PageResult<SearchResponse> response = bookQueryService.search(query, page, size);
-        DailyStat dailyStat = new DailyStat(query, LocalDateTime.now());
-        dailyStatCommandService.save(dailyStat);
+
+        if (!response.contents().isEmpty()) {
+            log.info("검색결과 개수: {}", response.size());
+            eventPublisher.publishEvent(new SearchEvent(query, LocalDateTime.now()));
+        }
         return response;
     }
 
